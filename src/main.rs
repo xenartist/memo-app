daf1134486e52f1287e83205e9a5a7c81ea4460e
xenartist <146194585+xenartist@users.noreply.github.com>
2025-03-26 22:13:04 +1100

    css style

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,14 @@ mod components;
 use dioxus::prelude::*;
 use crate::components::login::*;
 
+static MAIN_CSS: Asset = asset!("assets/main.css");
+static LOGIN_CSS: Asset = asset!("assets/login.css");
+
 #[component]
 fn App() -> Element {
     rsx! {
+        document::Stylesheet { href: MAIN_CSS }
+        document::Stylesheet { href: LOGIN_CSS }
         LoginPage {}
     }
 }
