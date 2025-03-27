8ed5ea487e982455dbf5a8c0e718feffe6ffb49c
xenartist <146194585+xenartist@users.noreply.github.com>
2025-03-27 15:41:17 +1100

    new wallet/import wallet

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,6 @@
 use leptos::prelude::*;
+use leptos::logging::log;
+use leptos::ev;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -9,9 +11,38 @@ extern "C" {
 
 #[component]
 pub fn App() -> impl IntoView {
+    // Handler functions for the buttons
+    let on_new_wallet = move |_: ev::MouseEvent| {
+        // TODO: Implement new wallet creation logic
+        log!("New Wallet button clicked");
+    };
+
+    let on_import_wallet = move |_: ev::MouseEvent| {
+        // TODO: Implement wallet import logic
+        log!("Import Wallet button clicked");
+    };
+
     view! {
         <main class="container">
-            <h1>"Memo App"</h1>
+            <div class="login-container">
+                <h1 class="app-title">"Memo App"</h1>
+                <div class="button-group">
+                    <button 
+                        class="wallet-btn new-wallet" 
+                        type="button"
+                        on:click=on_new_wallet
+                    >
+                        "New Wallet"
+                    </button>
+                    <button 
+                        class="wallet-btn import-wallet" 
+                        type="button"
+                        on:click=on_import_wallet
+                    >
+                        "Import Wallet"
+                    </button>
+                </div>
+            </div>
         </main>
     }
 }
