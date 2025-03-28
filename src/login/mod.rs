a5cbd5bc5f9efe824b654546940475e783d034c8
xenartist <146194585+xenartist@users.noreply.github.com>
2025-03-28 16:13:01 +1100

    rename

--- src/login/mod.rs
+++ src/login/mod.rs
@@ -1,13 +1,13 @@
-mod initial_step;
+mod login_initial_step;
 mod import_mnemonic_step;
 mod show_mnemonic_step;
 mod verify_mnemonic_step;
 mod set_password_step;
-mod complete_step;
+mod login_complete_step;
 
-pub use initial_step::InitialStep;
+pub use login_initial_step::InitialStep;
 pub use import_mnemonic_step::ImportMnemonicStep;
 pub use show_mnemonic_step::ShowMnemonicStep;
 pub use verify_mnemonic_step::VerifyMnemonicStep;
 pub use set_password_step::SetPasswordStep;
-pub use complete_step::CompleteStep; 
+pub use login_complete_step::CompleteStep; 
