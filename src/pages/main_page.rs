d1f7e69d59d98eef94aa2ef3725721cb5f3ba4c4
xenartist <146194585+xenartist@users.noreply.github.com>
2025-03-30 11:17:39 +1100

    add pubkey on main page

--- src/pages/main_page.rs
+++ src/pages/main_page.rs
@@ -6,6 +6,9 @@ pub fn MainPage() -> impl IntoView {
     let (version_status, set_version_status) = create_signal(String::from("Testing RPC connection..."));
     let (blockhash_status, set_blockhash_status) = create_signal(String::from("Getting latest blockhash..."));
     
+    // use a sample pubkey, we will get it from wallet state later
+    let pubkey = "7C4jsPZpht42Tw6MjXWF56Q5RQUocjBCAQS3DgXyLhyB";
+    
     // test rpc connection
     spawn_local(async move {
         let rpc = RpcConnection::new();
@@ -33,6 +36,17 @@ pub fn MainPage() -> impl IntoView {
 
     view! {
         <div class="main-page">
+            // top bar
+            <div class="top-bar">
+                <div class="wallet-address">
+                    <span class="address-label">"Wallet: "</span>
+                    <span class="address-value" title={pubkey}>
+                        {format!("{}...{}", &pubkey[..4], &pubkey[pubkey.len()-4..])}
+                    </span>
+                </div>
+            </div>
+
+            // rpc status
             <div class="rpc-status">
                 <h3>"X1 RPC Status"</h3>
                 <p>{version_status}</p>
