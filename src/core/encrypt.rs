c197512b50230b5baaaaf8fc7c3f66983845ecbf
xenartist <146194585+xenartist@users.noreply.github.com>
2025-04-18 12:37:44 +1000

    Update encrypt.rs

--- src/core/encrypt.rs
+++ src/core/encrypt.rs
@@ -7,7 +7,7 @@ use chacha20poly1305::{
     ChaCha20Poly1305,
 };
 use zeroize::{Zeroize, Zeroizing};
-use secrecy::Secret;
+use secrecy::{Secret, ExposeSecret};
 use rand::{rngs::OsRng, RngCore};
 
 use std::fmt;
