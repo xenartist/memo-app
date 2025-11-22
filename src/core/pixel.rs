70123717360ca1f5308f7ad00af0582509ee5e1b
xenartist <146194585+xenartist@users.noreply.github.com>
2025-11-22 21:40:18 +1100

    fix warning issue

--- src/core/pixel.rs
+++ src/core/pixel.rs
@@ -3,7 +3,6 @@ use flate2::write::DeflateEncoder;
 use flate2::read::DeflateDecoder;
 use base64::{encode, decode};
 use std::io::prelude::*;
-use rand::Rng;
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct Pixel {
@@ -120,7 +119,7 @@ impl Pixel {
         }
 
         if bit_count > 0 {
-            current_bits <<= (6 - bit_count);
+            current_bits <<= 6 - bit_count;
             result.push(Self::map_to_safe_char(current_bits));
         }
 
@@ -417,6 +416,7 @@ impl Default for Pixel {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rand::Rng;
 
     #[test]
     fn test_safe_string_conversion() {
